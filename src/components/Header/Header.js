cbfc40ce619c7c5b2598df097d0f581eba8cddac
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-06-02 15:16:48 +0300

    a few CSS change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header() {
   return (
-    <div style={{fontSize: 35, color: "grey", fontWeight: 400}}>Notes App</div>
+    <div style={{fontSize: 35, color: "grey", fontWeight: "bold", color: '#BF86BF'}}>Notes App</div>
   )
 }
 
